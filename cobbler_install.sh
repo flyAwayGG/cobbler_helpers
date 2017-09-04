f39d14fb5ec05cf1cd4b1b6e94de2f7e9c867f8d
flyAwayGG <flo2motion@gmail.com>
2017-09-04 10:06:23 +0300

    asd

--- cobbler_install.sh
+++ cobbler_install.sh
@@ -2,7 +2,7 @@
 
 yum -y install epel-release
 yum -y update
-yum -y install createrepo httpd mkisofs mod_wsgi mod_ssl python-cheetah python-netaddr python-simplejson python-urlgrabber PyYAML rsync syslinux tftp-server yum-utils pykickstart cobbler fence-agents wget vim tftp tcpdump net-tools xinetd nmap
+yum -y install createrepo httpd mkisofs mod_wsgi mod_ssl python-cheetah python-netaddr python-simplejson python-urlgrabber PyYAML rsync syslinux tftp-server yum-utils pykickstart cobbler fence-agents wget vim tftp tcpdump xinetd nmap
 
 # Disable Selinux in /etc/sysconfig/selinux
 # Enable TFTP in /etc/xinetd.d/tftp
@@ -15,20 +15,20 @@ cp /usr/share/syslinux/pxelinux.0 /var/lib/cobbler/loaders/
 #cp /usr/share/syslinux/chain.c32 /var/lib/tftpboot
 #cp /usr/share/syslinux/vesamenu.c32 /var/lib/tftpboot
 
-# echo "PROMPT 0
-# MENU TITLE Cobbler | http://cobbler.github.io/
-# TIMEOUT 10
-# TOTALTIMEOUT 6000
-# ONTIMEOUT \$pxe_timeout_profile
+echo "PROMPT 0
+MENU TITLE Cobbler | http://cobbler.github.io/
+TIMEOUT 30
+TOTALTIMEOUT 6000
+ONTIMEOUT \$pxe_timeout_profile
 
-# LABEL local
-#         MENU LABEL (local)
-#         MENU DEFAULT
-#         LOCALBOOT -1
+LABEL local
+        MENU LABEL (local)
+        MENU DEFAULT
+        LOCALBOOT -1
         
-# \$pxe_menu_items
+\$pxe_menu_items
 
-# MENU end" > /etc/cobbler/pxe/pxedefault.template
+MENU end" > /etc/cobbler/pxe/pxedefault.template
 
 
 systemctl start httpd
