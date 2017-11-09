1cca67047efd0e3e7f1f8a65af24b3bcf747f3a7
root <root@localhost.localdomain>
2017-11-09 11:42:01 +0300

    change cobbler install

--- cobbler_install.sh
+++ cobbler_install.sh
@@ -10,26 +10,14 @@ yum -y install createrepo httpd mkisofs mod_wsgi mod_ssl python-cheetah python-n
 # In /etc/cobbler/settings : 
 # Set server : current ip 
 # Set pxe_just_once: 1
+# Decrease TIMEOUT in /etc/cobbler/pxe/pxedefault.template 
 
 cp /usr/share/syslinux/pxelinux.0 /var/lib/cobbler/loaders/
 #cp /usr/share/syslinux/chain.c32 /var/lib/tftpboot
 #cp /usr/share/syslinux/vesamenu.c32 /var/lib/tftpboot
 
-echo "PROMPT 0
-MENU TITLE Cobbler | http://cobbler.github.io/
-TIMEOUT 30
-TOTALTIMEOUT 6000
-ONTIMEOUT \$pxe_timeout_profile
-
-LABEL local
-        MENU LABEL (local)
-        MENU DEFAULT
-        LOCALBOOT -1
-        
-\$pxe_menu_items
-
-MENU end" > /etc/cobbler/pxe/pxedefault.template
-
+# Change ip to static
+# Reboot
 
 systemctl start httpd
 systemctl enable httpd
@@ -44,9 +32,7 @@ systemctl start xinetd
 systemctl enable xinetd
 
 systemctl disable firewalld
-systemctl stop firewalld
-
-cobbler get-loaders
+systemctl stop firewall
 
 systemctl start cobbler
 systemctl enable cobbler
@@ -54,5 +40,6 @@ systemctl enable cobbler
 systemctl start cobblerd
 systemctl enable cobblerd
 
-
+cobbler get-loaders
 cobbler sync
+
